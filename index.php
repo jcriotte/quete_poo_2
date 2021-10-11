0f9dcd2eab60b17a75d697850f0f103f6b96c383
RIOTTE <jcriotte69@gmail.com>
2021-10-11 20:34:20 +0200

    nouvelle version

--- index.php
+++ index.php
@@ -1,4 +1,57 @@
 <?php
 
-// First Labour : Heracles vs Nemean Lion
+// First Labour : Heracles vs Nemean Lion   <link href="style.css" rel="stylesheet" type="text/css">
 // use your Figher class here
+require_once ('src/Fighter.php');
+
+$heracles = new Fighter('👨 Hercules', 20 , 6);
+$lionDeMenes = new Fighter('🎃 Lion de Némée', 11 , 13);
+
+//echo $heracles->getName() . " " . $heracles->getLife() . " " . $heracles->getStrength() . " " . $heracles->getDexterity();
+//echo $lionDeMenes->getName() . " " . $lionDeMenes->getLife() . " " . $lionDeMenes->getStrength() . " " . $lionDeMenes->getDexterity();
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta charset="utf-8">
+  <title>My test page</title>
+
+</head>
+
+<body>
+    <h1>Bienvenu dans la bagare !</h1>
+    <p><?php echo $heracles->getName() . " " . $heracles->getLife() . " " . $heracles->getStrength() . " " . $heracles->getDexterity() ?> </p>
+    <p><?php echo $lionDeMenes->getName() . " " . $lionDeMenes->getLife() . " " . $lionDeMenes->getStrength() . " " . $lionDeMenes->getDexterity() ?> </p>
+
+<?php
+$numberRound = 1;
+while (($heracles->getLife() > 0) && ($lionDeMenes->getLife() > 0)) {
+    ?>
+    <p><?php echo "⏱️ Round : " . $numberRound;?></p>
+    <?php
+    $heracles->fight($lionDeMenes);
+    ?>
+    <p><?php echo $heracles->getName() . " 🗡️ " . $lionDeMenes->getName() . " 💙 " . $lionDeMenes->getName() . " : " . $lionDeMenes->getLife()?> </p>
+    <?php
+    $lionDeMenes->fight($heracles);
+    ?>
+    <p><?php echo $lionDeMenes->getName() . " 🗡️ " . $heracles->getName() . " 💙 " . $heracles->getName() . " : " . $heracles->getLife()?> </p>
+    <?php $numberRound++;
+}
+if ($heracles->getLife() > 0) {
+    ?>
+    <h2><?php echo "💀 " . $lionDeMenes->getName() . " is dead";?> </h2><br>
+    <h2><?php echo "🏆 " . $heracles->getName() . " win (💙 : " . $heracles->getLife() . ")";?> </h2><br>
+    <?php
+} else {
+    ?>
+    <h2><?php echo "💀 " . $heracles->getName() . " is dead";?> </h2><br>
+    <h2><?php echo "🏆 " . $lionDeMenes->getName() . " win (💙 : " . $lionDeMenes->getLife() . ")";?> </h2><br>
+    <?php
+}
+?>
+</body>
+
+</html>
+
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,3 +1,57 @@
 <?php
 
 /* Fighter class definition */
+class Fighter {
+    const MAX_LIFE = 100;
+    private string $name;
+    private int $strength;
+    private int $dexterity;
+    private int $life;
+
+    public function __construct(string $name, int $strength, int $dexterity)
+    {
+        $this->name = $name;
+        $this->strength = $strength;
+        $this->dexterity = $dexterity;
+        $this->life = Fighter::MAX_LIFE;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getStrength(): int {
+        return $this->strength;
+    }
+
+    public function getDexterity(): int {
+        return $this->dexterity;
+    }
+
+    public function getLife(): int {
+        return $this->life;
+    }
+
+    public function fight(Fighter $ennemi): void {
+        echo $this->name . " " . $this->strength;
+        $numberDegat = rand(1, $this->strength);
+        echo " attaque initiale de : $numberDegat";
+        echo " dexterite de ennemi : $ennemi->dexterity";
+        if ($ennemi->dexterity < $numberDegat) {
+            $numberDegat = $numberDegat - $ennemi->dexterity;
+        } else {
+            $numberDegat = 0;
+        }
+        if ($numberDegat < 0) {
+            $numberDegat = 0;
+        }
+        echo " attaque corrigée : $numberDegat";
+        if ($ennemi->life > $numberDegat) {
+            $ennemi->life = $ennemi->life - $numberDegat;
+        } else {
+            $ennemi->life = 0;
+        }
+    }
+}
+
+?>
